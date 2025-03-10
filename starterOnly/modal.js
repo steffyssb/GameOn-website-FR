ca383f7f7b5bdbfb5921971c529505db16219198
steffyssb <steffyssb6492@gmail.com>
2025-03-10 08:23:21 +0100

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closebtn = document.querySelectorAll(".content .close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closebtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
